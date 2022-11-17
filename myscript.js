15af4b30883354598cd2495198d4c2b62a0e7a77
analogsoul13 <analogsoul13@gmail.com>
2022-11-17 19:42:45 +0530

    Initial commit

---
+++ myscript.js
@@ -0,0 +1,5 @@
+document.getElementById("text").innerHTML = "Hello world";
+function myFunction(){
+    document.getElementById("text").innerHTML ="Here is the text appeared when you called the function.";
+}
+
